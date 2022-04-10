4961fce386261e39daf87c8a068d7a7d64ea1429
SiraajBeford <92030563+SiraajBeford@users.noreply.github.com>
2022-04-10 13:49:04 +0200

    Data cleaning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from soupsieve import select
 from src.data_cleaning import DataCleaner
 
 # Clean the data
 cleaner = DataCleaner()
 cleaner.clean_all_schedules()
+cleaner.print_current_data()
 cleaner.export_cleaned_schedules()
--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,4 +1,5 @@
 from distutils import extension
+from matplotlib.pyplot import axis
 import pandas as pd
 import numpy as np
 import os
@@ -47,22 +48,42 @@ class DataCleaner(DataCombiner):
             self.cleaned_data_list.append(self.clean_schedule(schedule))
 
     def clean_schedule(self,schedule):
-        return self.remove_if_empty_size(self.clean_empty_rows(schedule)) 
+        schedule = self.clean_empty_rows(schedule)
+        schedule = self.remove_if_empty_size(schedule)
+        schedule = self.clean_units(schedule)
+        schedule = self.clean_area_and_surface_area_zero(schedule)
+
+        return schedule
 
     def clean_empty_rows(self, schedule):
         empty_row_indices = schedule[schedule.isna().all(axis=1)].index
         schedule = schedule.drop(index=empty_row_indices)
         return schedule
 
+    def clean_units(self, schedule):
+        schedule["Area"] = schedule["Area"].astype(str)
+        schedule["Area"] = schedule["Area"].str.replace(" m²","")
+        schedule["Surface Area"] = schedule["Surface Area"].astype(str)
+        schedule["Surface Area"] = schedule["Surface Area"].str.replace(" m²","")
+        return schedule
+
+    def clean_area_and_surface_area_zero(self, schedule):
+        schedule["Area"] = schedule["Area"].astype(float)
+        schedule["Surface Area"] = schedule["Surface Area"].astype(float)
+        zero_indices_area = schedule.index[schedule['Area']==0.0]
+        zero_indices_surface_area = schedule.index[schedule['Surface Area']==0.0]
+        common_indices = list(set(zero_indices_area).intersection(zero_indices_surface_area))
+        schedule = schedule.drop(index=common_indices)
+        return schedule
+
     def pack_results(self):
         self.export_schedules = dict(zip(self.used_files, self.cleaned_data_list))
 
     def export_cleaned_schedules(self):
+        self.check_if_dir_empty()
         self.pack_results()
-        # print(self.export_schedules)
         for key, value in self.export_schedules.items():
-            value.to_csv(self.export_path_for_schedules + '\\' + key, encoding='utf-8-sig')
-
+            value.to_csv(self.export_path_for_schedules + '\\' + key, encoding='utf-8-sig', index = False)
 
     def remove_if_empty_size(self, schedule):
         no_size_rows_indices = schedule[schedule['Size'].isna()].index
@@ -75,3 +96,13 @@ class DataCleaner(DataCombiner):
     def print_current_data(self):
         print(self.cleaned_data_list[0])
 
+    def check_if_dir_empty(self):
+        if len(os.listdir(self.export_path_for_schedules) ) == 0:
+            pass
+        else:    
+            for file_name in os.listdir(self.export_path_for_schedules):
+                # construct full file path
+                file = self.export_path_for_schedules + file_name
+                if os.path.isfile(file):
+                    print('Deleting file:', file)
+                    os.remove(file)
